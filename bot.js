597526d25d887f47bc1b50bdbb02c24ae1909ebd
megafreezer <36858947+megafreezer@users.noreply.github.com>
2018-09-03 21:55:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -54,7 +54,7 @@ if(message.content.startsWith(p + `help`)) {
 }
     if(['hug', 'обнимахи'].includes(command)) {
           const u = msg.mentions.users.first()
-            const page = await r.get('https://nekos.life/api/v2/img/hug')
+            const page = r.get('https://nekos.life/api/v2/img/hug')
             const embed = new Discord.RichEmbed()
             .setTitle(message.author + ' kissed ' + u.tag)
             .setImage(page.body.url)
