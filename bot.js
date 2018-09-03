53df69cedf0c1c58db509916b32931b6f79654aa
megafreezer <36858947+megafreezer@users.noreply.github.com>
2018-09-03 18:11:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,35 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client()
+const Discord = require(`discord.js`);
+const bot = new Discord.Client();
 bot.login(process.env.TOKEN)
- 
-let p = '##'
-let color = '#615172'
-let bot_name = 'Volts'
-
-bot.on('message', message => {
-  if(message.author.bot) return
-  if(message.content.indexOf(client.p) !== 0) return
-  let args = message.content.slice(client.p.length).trim().split(/ +/g)
-  let cmd = args.shift().toLowerCase()
-
-  try { let req = require(`./cmds/${cmd}.js`) }
-  catch(e) { return console.log(e) }
-}
+//Let
+let p ='##';
+let color = '#615172';
+let bot_name = "Volts";
+//end Let
+//Log
+bot.on('ready', () => {
+    bot.user.setPresence({ game: { name: `##help`, type: 0 } }).catch();
 });
-
+bot.on('ready', () => {
+    console.log("<---->Я готов<---->")
+    console.log(`Я на ${bot.guilds.size} серверах`)
+    console.log(`Каналов/категорий ${bot.channels.size}`)
+    console.log(`Юзеров ${bot.users.size}`)
+    console.log(`<---->Бот<---->`)
+    console.log(`Название бота ${bot.user.username}`)
+    console.log(`Айди бота: ${bot.user.id}`)
+    console.log("<----------------->");
+//end Log
+bot.on('message', message => {
+    const args = message.content.slice(p.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+if(message.content.startsWith(p + `help`)) {
+    const embed = new Discord.RichEmbed()
+        .setTitle("Help you")
+        .setColor(color)
+        .setDescription('prefix bot- `##`\n\n ##help - **bot help you**')
+        .setFooter(bot_name) 
+        .setTimestamp();
+    message.channel.send({embed});
+}
+})});
