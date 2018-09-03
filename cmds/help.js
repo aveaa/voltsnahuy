14bf804bfccbe6f26b6ad4499154fe3fb911affe
megafreezer <36858947+megafreezer@users.noreply.github.com>
2018-09-03 18:11:01 +0300

    Delete help.js

--- cmds/help.js
+++
@@ -1,9 +0,0 @@
-if(message.content.startsWith(p + `help`)) {
-    const embed = new Discord.RichEmbed()
-        .setTitle("Help for you.")
-        .setColor(color)
-        .setDescription('**Bot prefix - ' + p + '**')
-        .addField('Fun', '##help - help for you.')
-        .setFooter(bot_name) 
-        .setTimestamp();
-    message.channel.send({embed});
