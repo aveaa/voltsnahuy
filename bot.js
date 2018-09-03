1be48251175bbdf359636acb12e7415256552ef7
megafreezer <36858947+megafreezer@users.noreply.github.com>
2018-09-03 18:02:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,18 +7,13 @@ let color = '#615172'
 let bot_name = 'Volts'
 
 bot.on('message', message => {
-    const args = message.content.slice(p.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-    
-if(message.content.startsWith(p + `help`)) {
-    const embed = new Discord.RichEmbed()
-        .setTitle("Help for you.")
-        .setColor(color)
-        .setDescription('**Bot prefix - ' + p + '**')
-        .addField('Fun', 'sukablet')
-        .setFooter(bot_name) 
-        .setTimestamp();
-    message.channel.send({embed});
+  if(message.author.bot) return
+  if(message.content.indexOf(client.p) !== 0) return
+  let args = message.content.slice(client.p.length).trim().split(/ +/g)
+  let cmd = args.shift().toLowerCase()
+
+  try { let req = require(`./cmds/${cmd}.js`) }
+  catch(e) { return console.log(e) }
 }
 });
 
