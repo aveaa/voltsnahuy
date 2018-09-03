13f8d8af507adddc24b48f176227d68d2be21f02
megafreezer <36858947+megafreezer@users.noreply.github.com>
2018-09-03 21:37:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -52,12 +52,4 @@ if(message.content.startsWith(p + `help`)) {
                 message.react("✅")
             }).catch(function() {});
 }
-            
-        if(['suka, blet'].includes(command)) {
-            message.channel.send("suka");
-            ('messageReactionAdd', (reaction, user) => {
-    if(reaction.emoji.name === "✅") {
-        message.channel.send("suka");
-
-}
-
+})});
