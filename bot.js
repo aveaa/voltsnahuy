8f8b0fdb50bf532315deeafbb402b312c7b93cbb
megafreezer <36858947+megafreezer@users.noreply.github.com>
2018-09-03 17:44:32 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client()
+bot.login(process.env.TOKEN)
+ 
+let p = '##'
+let color = '#615172'
+let bot_name = 'Volts'
+
+bot.on('message', message => {
+    const args = message.content.slice(p.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    
+if(message.content.startsWith(p + `help`)) {
+    const embed = new Discord.RichEmbed()
+        .setTitle("Help for you.")
+        .setColor(color)
+        .setDescription('**Bot prefix - ' + p)
+        .addField('Fun', 'sukablet')
+        .setFooter(bot_name) 
+        .setTimestamp();
+    message.channel.send({embed});
+}
+});
+
